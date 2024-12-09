193f42f6340c1660673d8c4dc010158b6b9cb1a9
Daniele Guffanti <daniele.guffanti@cern.ch>
2024-12-09 11:21:17 +0100

    reads file list

--- Analysis/neutron_spectrum_prova.cc
+++ Analysis/neutron_spectrum_prova.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include "TFile.h"
 #include "TTree.h"
@@ -17,12 +18,8 @@ struct TrkEvent_t {
   void clear() {_energy->clear(); _weight->clear();}
 };
 
-int neutron_spectrum_prova(const TString file_path1, TString file_path2)
+int neutron_spectrum_prova(const TString file_list_path)
 {
-  TFile* file1 = new TFile(file_path1, "READ");
-  TFile* file2 = new TFile(file_path2, "READ");
-
-  std::vector<TString> files = {file_path1, file_path2};
   std::vector<TString> layer_names = {
     "water_PV", "steel0_PV", "plywood0_PV", "borpol0_PV", "plywood1_PV", 
     "poly0_PV", "plywood2_PV", "steel1_PV", "plywood3_PV", "poly1_PV",
@@ -31,48 +28,69 @@ int neutron_spectrum_prova(const TString file_path1, TString file_path2)
 
   std::string branch1 = "AbsEnergy";
   std::string branch2 = "TrackWeight";
-  std::vector<std::vector<TH1D*>> layer_hists(2, std::vector<TH1D*>(layer_names.size(), nullptr));
-  std::vector<std::vector<TH1D*>> layer_hists_weights(2, std::vector<TH1D*>(layer_names.size(), nullptr));
-  //std::vector<TrkEvent_t> layer_events(layer_names.size());
+  std::vector<TH1D*> layer_hists(layer_names.size(), nullptr);
+  std::vector<TH1D*> layer_hists_weights(layer_names.size(), nullptr);
+  std::vector<TChain> chains(layer_names.size());
+  std::vector<TrkEvent_t> layer_events(layer_names.size());
 
-  //std::vector<TChain*> layer_chain(layer_names.size(), nullptr);
-  
+  std::vector<TString> files;
+  std::ifstream file_list;
+  file_list.open(file_list_path);
+  if (!file_list.is_open()) {
+    std::cerr << "Error: could not open file list" << std::endl;
+    return 1;
+  }
+  std::string line; 
+  while (std::getline(file_list, line)) {
+    files.push_back(line);
+  }
+  file_list.close();
+
   //Creating histos into every chain
   for (int i = 0; i < layer_names.size(); i++) {
     TString layer = layer_names[i];
+    TH1D* layer_hist = new TH1D (Form("%s", layer.Data()), layer, 100, 0, 20);
+    TH1D* layer_hist_weight = new TH1D(Form("%s_w", layer.Data()), layer, 10000, 0, 1);
+
+    layer_hists[i] = layer_hist;
+    layer_hists_weights[i] = layer_hist_weight;
+
+    chains[i].SetName("Shielding_"+layer);
+
     for (int k = 0; k < files.size(); k++) {
-      TChain chain ("Shielding_" + layer);
-      chain.Add(files[k]);
-      TH1D* layer_hist = new TH1D (Form("%s_%d", layer.Data(), k), layer, 100, 0, 20);
-      TH1D* layer_hist_weight = new TH1D(Form("%s_w_%d", layer.Data(), k), layer, 10000, 0, 1);
-      std::vector<double>* energy = nullptr;
-      std::vector<double>* weight = nullptr;
-      chain.SetBranchAddress(branch1.c_str(), &energy);
-      chain.SetBranchAddress(branch2.c_str(), &weight);
-      int n_entries = chain.GetEntries();
-      for (int s = 0; s < n_entries; s++) {
-        chain.GetEntry(s);
-        for (int s = 0; s < energy->size(); s ++) {
-          layer_hist->Fill((*energy)[i], (*weight)[i]);
-          layer_hist_weight->Fill((*weight)[i]);
-        }
-        
+      chains[i].Add(files[k]);
+    }
+
+    chains[i].SetBranchAddress(branch1.c_str(), &layer_events[i]._energy);
+    chains[i].SetBranchAddress(branch2.c_str(), &layer_events[i]._weight);
+  }
+
+  for (const auto& chain : chains) {
+    printf("Chain %s has %lli entries\n", chain.GetName(), chain.GetEntries());
+  }
+
+  const Long64_t n_entries = chains[0].GetEntries();
+  for (int s = 0; s < n_entries; s++) {
+    int i=0; 
+    for (auto& chain : chains) {
+      layer_events[i].clear();
+      chain.GetEntry(s);
+      auto& energy = layer_events[i]._energy;
+      auto& weight = layer_events[i]._weight;
+      for (int s = 0; s < energy->size(); s ++) {
+        layer_hists[i]->Fill((*energy)[s], (*weight)[s]);
+        layer_hists_weights[i]->Fill((*weight)[s]);
       }
-      layer_hists[k][i] = layer_hist;
-      layer_hists_weights[k][i] = layer_hist_weight;
+      i++; 
     }
   }
   //Creating canvas to show everything
   TCanvas* canvas = new TCanvas("canvas", "Histos Comparison", 1600, 1200);
-  canvas->Divide(4, 4);
+  canvas->DivideSquare(layer_names.size());
   for (int i = 0; i < layer_names.size(); i++) {
-    int canvas_pad_left = i * 2 + 1;
-    //int canvas_pad_right = canvas_pad_left + 1;
-    for (int k = 0; k < files.size(); k++) {
-      canvas->cd(canvas_pad_left+k);
-      layer_hists[k][i]->SetLineColor(k+1);
-      layer_hists[k][i]->Draw("HIST");
-    }
+    canvas->cd(i+1);
+    layer_hists[i]->SetLineColor(i+1);
+    layer_hists[i]->Draw("HIST");
   }
   canvas->SaveAs("Comparison Energy Weighted.png");
   return 0;
